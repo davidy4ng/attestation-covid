efe9349466852f31501425c5d6ae3f1f42f77eb3
TheTrueTom <thomas@brichart.fr>
2020-10-31 09:15:51 +0100

    Update copyright on created file

--- AttestationCovid/Form/Cells/DateCell.swift
+++ AttestationCovid/Form/Cells/DateCell.swift
@@ -3,7 +3,7 @@
 //  AttestationCovid
 //
 //  Created by Thomas Brichart on 31/10/2020.
-//  Copyright © 2020 David Yang. All rights reserved.
+//  Copyright © 2020 Thomas Brichart. All rights reserved.
 //
 
 import UIKit
